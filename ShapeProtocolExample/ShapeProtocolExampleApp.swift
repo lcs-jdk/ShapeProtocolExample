0005cda1493e26baf4e9694872f234b378a0efa7
Jayden Kim <hkim@lcs.on.ca>
2024-12-13 14:36:30 -0500

    Initial Commit

---
+++ ShapeProtocolExample/ShapeProtocolExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeProtocolExampleApp.swift
+//  ShapeProtocolExample
+//
+//  Created by kim heejun on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ShapeProtocolExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
